94fa6b8c17379bbc35c178a1a7cfd8ab1e662b4b
gazza121212 <gazzatube@gmail.com>
2018-01-24 23:28:28 +1100

    moved some stuff for the encoder,builds

--- software/cpp/2018.Luigi/src/Subsystems/SubEncodedArmLift.cpp
+++ software/cpp/2018.Luigi/src/Subsystems/SubEncodedArmLift.cpp
@@ -16,26 +16,7 @@ SubEncodedArmLift::SubEncodedArmLift() : Subsystem("ExampleSubsystem") {
 
 	targetPositionRotations = 0.0;
 
-	/* lets grab the 360 degree position of the MagEncoder's absolute position */
-	absolutePosition = _talon->GetSelectedSensorPosition(0) & 0xFFF; /* mask out the bottom12 bits, we don't care about the wrap arounds */
-		/* use the low level API to set the quad encoder signal */
-	_talon->SetSelectedSensorPosition(absolutePosition, kPIDLoopIdx, kTimeoutMs);
 
-	/* choose the sensor and sensor direction */
-	_talon->ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, kPIDLoopIdx, kTimeoutMs);
-	_talon->SetSensorPhase(false);
-
-	/* set the peak and nominal outputs, 12V means full */
-	_talon->ConfigNominalOutputForward(0, kTimeoutMs);
-	_talon->ConfigNominalOutputReverse(0, kTimeoutMs);
-	_talon->ConfigPeakOutputForward(1, kTimeoutMs);
-	_talon->ConfigPeakOutputReverse(-1, kTimeoutMs);
-
-	/* set closed loop gains in slot0 */
-	_talon->Config_kF(kPIDLoopIdx, 0.0, kTimeoutMs);
-	_talon->Config_kP(kPIDLoopIdx, 0.1, kTimeoutMs);
-	_talon->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
-	_talon->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
 
 }
 
@@ -71,7 +52,8 @@ void SubEncodedArmLift::ArmToScalePos() {  //Button 12
     FOUR++;
 
 	//targetPositionRotations = (_prefs->GetDouble("Scale Position", 0.0))*4096;
-	targetPositionRotations = 2.0 * 4096;
+//	targetPositionRotations = 2.0 * 4096;
+	targetPositionRotations = 2048;
 	_talon->Set(ControlMode::Position, targetPositionRotations);
 
 
