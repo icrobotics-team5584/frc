8efe04d5f709e0e1dc7aae1a04e5a064951daa7c
gazza121212 <gazzatube@gmail.com>
2018-01-27 17:02:09 +1100

    started control change

--- software/cpp/2018.Luigi/src/OI.cpp
+++ software/cpp/2018.Luigi/src/OI.cpp
@@ -14,28 +14,28 @@ OI::OI() {
 	joystick0.reset(new frc::Joystick(0));
 
     //Initiate Intake button
-    btnIntake.reset(new JoystickButton(joystick0.get(), 2));
-    btnIntake->WhileHeld(new CmdIntake());
+	btnIntake.reset(new JoystickButton(joystick0.get(), 2));
+	btnIntake->WhileHeld(new CmdOutput());
 
     //Initiate Output button
     btnOutput.reset(new JoystickButton(joystick0.get(), 1));
 	btnOutput->WhileHeld(new CmdOutput());
 
 	//Initiate Arm Encoded Actuator controls
-	btnArmToGround.reset(new JoystickButton(joystick0.get(), 10));
+	btnArmToGround.reset(new JoystickButton(joystick0.get(), 1));
 	btnArmToGround->WhenPressed(new CmdArmPosGround());
-	btnArmToSwitch.reset(new JoystickButton(joystick0.get(), 11));
+	btnArmToSwitch.reset(new JoystickButton(joystick0.get(), 3));
 	btnArmToSwitch->WhenPressed(new CmdArmPosSwitch());
-	btnArmToScale.reset(new JoystickButton(joystick0.get(), 12));
+	btnArmToScale.reset(new JoystickButton(joystick0.get(), 4));
 	btnArmToScale->WhenPressed(new CmdArmPosScale());
 
 	//Motion Profile test
-	btnMP.reset(new JoystickButton(joystick0.get(), 4));
+	btnMP.reset(new JoystickButton(joystick0.get(), 44));
 	// btnMP->ToggleWhenPressed(new CmdAutoMotionProfileTest());
 	btnMP->WhileHeld(new CmdAutoMotionProfileTest());
 
 	//Initiate Change camera button
-	btnChangeCamera.reset(new JoystickButton(joystick0.get(), 9));
+	btnChangeCamera.reset(new JoystickButton(joystick0.get(), 99));
 	btnChangeCamera->WhenPressed(new CmdChangeCamera());
 
 }
