95dfd196e298502bd1ad0ef7d3677641eccbbc36
gazza121212 <gazzatube@gmail.com>
2018-01-29 23:39:39 +1100

    should be working but it isent

--- software/cpp/2018.Luigi/src/Commands/MyJoystickDrive.cpp
+++ software/cpp/2018.Luigi/src/Commands/MyJoystickDrive.cpp
@@ -3,6 +3,7 @@
 MyJoystickDrive::MyJoystickDrive(): frc::Command() {
     //requires(Robot::chassis.get());
 	Requires(Robot::subDriveBase.get());
+	Requires(Robot::subEncodedArmLift.get());
 }
 
 // Called just before this Command runs the first time
@@ -13,6 +14,7 @@ void MyJoystickDrive::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void MyJoystickDrive::Execute() {
     Robot::subDriveBase->TakeJoystickInputs(Robot::oi->getJoystick0());
+    Robot::subEncodedArmLift->TakeJoystickInputs(Robot::oi->getJoystick0());
 }
 
 // Make this return true when this Command no longer needs to run execute()
