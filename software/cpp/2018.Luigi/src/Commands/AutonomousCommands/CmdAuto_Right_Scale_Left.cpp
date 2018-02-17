1aca7a7b9b5deeef0c60a2b771aca100ee277c95
Liam <liam.stow@outlook.com>
2018-02-17 21:40:27 +1100

    Added Right-Scale-Left routine using navx

--- software/cpp/2018.Luigi/src/Commands/AutonomousCommands/CmdAuto_Right_Scale_Left.cpp
+++ software/cpp/2018.Luigi/src/Commands/AutonomousCommands/CmdAuto_Right_Scale_Left.cpp
@@ -1,5 +1,11 @@
 #include "CmdAuto_Right_Scale_Left.h"
+#include "Commands/CmdArmPosExchange.h"
+#include "Commands/CmdGyroDrive.h"
 
 CmdAuto_Right_Scale_Left::CmdAuto_Right_Scale_Left() {
-
+	AddParallel(new CmdArmPosExchange());
+	AddSequential(new CmdGyroDrive(6,0,false,false), 5);
+	AddSequential(new CmdGyroDrive(0,-90,false,true), 2);
+	AddSequential(new CmdGyroDrive(4, 0, true, false), 5);
+	AddSequential(new CmdGyroDrive(0, 0, false, true), 2);
 }
