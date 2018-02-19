7fd5391916b7c5221533b48e02e0412d793d2dee
gazza121212 <gazzatube@gmail.com>
2018-02-19 21:38:20 +1100

    removed obsolete files, commented out cout's ,tested with 3.3 works

--- software/cpp/2018.Luigi/src/Subsystems/SubIntake.cpp
+++ software/cpp/2018.Luigi/src/Subsystems/SubIntake.cpp
@@ -18,11 +18,11 @@ void SubIntake::InitDefaultCommand() {
 void SubIntake::Periodic() {
 	if( fingerDesiredPositionUp ) {
 		if( swtFingerLimit->Get() ) {
-			std::cout << "SubIntake::Periodic: Switch Desired Position UP and switch TRUE" << std::endl;
+			//std::cout << "SubIntake::Periodic: Switch Desired Position UP and switch TRUE" << std::endl;
 			// finger is up and in the correct position so stop the motor
 			spFinger->Set(0.0);
 		} else {
-			std::cout << "SubIntake::Periodic: Switch Desired Position UP and switch FALSE" << std::endl;
+			//std::cout << "SubIntake::Periodic: Switch Desired Position UP and switch FALSE" << std::endl;
 			// finger is not in the up position so set it off in that direction
 			spFinger->Set(-1.0);
 		}
@@ -30,13 +30,13 @@ void SubIntake::Periodic() {
 
 	if( ! fingerDesiredPositionUp ) {
 		if( swtFingerLimit->Get() ) {
-			std::cout << "SubIntake::Periodic: Switch Desired Position DOWN and switch TRUE" << std::endl;
+			//std::cout << "SubIntake::Periodic: Switch Desired Position DOWN and switch TRUE" << std::endl;
 			// we need to launch the finger down movement
 			fingerTicks = 0;
 			spFinger->Set(1.0);
 
 		} else {
-			std::cout << "SubIntake::Periodic: Switch Desired Position DOWN and switch FALSE" << std::endl;
+			//std::cout << "SubIntake::Periodic: Switch Desired Position DOWN and switch FALSE" << std::endl;
 			// the movement is in progress or the finger is already down, lets check if we are there yet ...
 			if( fingerTicks >= fingerTicksToMove ){
 				// we have moved far enough so lets hold up here ...
