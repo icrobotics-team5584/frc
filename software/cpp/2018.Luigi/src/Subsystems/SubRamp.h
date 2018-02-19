e62c7894c984bc21584215e2c158e7e9a7992611
gazza121212 <gazzatube@gmail.com>
2018-02-20 00:55:59 +1100

    IT WORKS WITH REBOOTgit add -A

--- software/cpp/2018.Luigi/src/Subsystems/SubRamp.h
+++ software/cpp/2018.Luigi/src/Subsystems/SubRamp.h
@@ -16,7 +16,7 @@ private:
 	int _scaleTarget = 160;  //wait for scale to go up
 
 	int _dropCount = 0;
-	int _dropTarget = 11; //ramp motor relase time
+	int _dropTarget = 14; //ramp motor relase time
 
 	int _climbCount = 0;
 	int _climbTarget = 130; //wait for ramp drop time
