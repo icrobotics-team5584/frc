3613b3054a2651293fdedd6f99c6122091b3b43a
gazza121212 <gazzatube@gmail.com>
2018-03-18 14:22:37 +1100

    liam,, fixed the auto clash with barker

--- software/cpp/2018.Luigi/src/Commands/AutonomousCommands/CmdAuto_BaselineRight.cpp
+++ software/cpp/2018.Luigi/src/Commands/AutonomousCommands/CmdAuto_BaselineRight.cpp
@@ -6,5 +6,4 @@ CmdAuto_BaselineRight::CmdAuto_BaselineRight() {
 	AddParallel(new CmdArmPosSwitch());
 	AddSequential(new CmdGyroDrive(5.5, 0), 3);
 	AddSequential(new CmdGyroDrive(0, 90, true), 2);
-	AddSequential(new CmdGyroDrive(4, 90));
 }
