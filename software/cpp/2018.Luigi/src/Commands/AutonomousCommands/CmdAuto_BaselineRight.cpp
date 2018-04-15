bb8357044ec9d2e4fc2dde68d97a9be92f5b02f2
Liam <liam.stow@outlook.com>
2018-04-15 23:37:29 +1000

    Task 4: Added stright drive to end of BaselineRight

--- software/cpp/2018.Luigi/src/Commands/AutonomousCommands/CmdAuto_BaselineRight.cpp
+++ software/cpp/2018.Luigi/src/Commands/AutonomousCommands/CmdAuto_BaselineRight.cpp
@@ -6,4 +6,5 @@ CmdAuto_BaselineRight::CmdAuto_BaselineRight() {
 	AddParallel(new CmdArmPosSwitch());
 	AddSequential(new CmdGyroDrive(5.5, 0), 3);
 	AddSequential(new CmdGyroDrive(0, 90, true), 2);
+	AddSequential(new CmdGyroDrive(1, 90), 2);	//Houston task 4: End on a 1m straight drive
 }
