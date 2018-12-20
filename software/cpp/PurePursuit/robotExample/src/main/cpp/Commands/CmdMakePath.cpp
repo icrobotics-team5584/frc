9d213a16d47b24f165a5918069b49ce727e119d9
Liam Stow <liam.stow@outlook.com>
2018-12-20 18:10:28 +1100

    making path: Now displays path for testing

--- software/cpp/PurePursuit/robotExample/src/main/cpp/Commands/CmdMakePath.cpp
+++ software/cpp/PurePursuit/robotExample/src/main/cpp/Commands/CmdMakePath.cpp
@@ -1,13 +1,19 @@
 #include "Commands/CmdMakePath.h"
 #include "purePursuit/include/pathFollower.hpp"
 #include "purePursuit/include/Point.hpp"
-
+#include <vector>
+#include <iostream>
 
 CmdMakePath::CmdMakePath() {
   // Use Requires() here to declare subsystem dependencies
   // eg. Requires(Robot::chassis.get());
   PathFollower pf;
-  //vector<Point> path = pf.constructVectorPath("../../include/purePursit/examplePaths/test_left.csv");
+  
+  vector<Point> path = pf.constructVectorPath("../../include/purePursit/examplePaths/test_left.csv");
+
+  for (Point point : path) {
+    cout << point.x << point.y << point.velocity << endl;
+  }
 }
 
 // Called just before this Command runs the first time
