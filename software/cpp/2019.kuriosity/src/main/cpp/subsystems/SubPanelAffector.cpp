1a1a5f114fa4f4061bf3d751117f7053628d42c3
Unknown <kikidunn2002@gmail.com>
2019-01-30 20:28:48 +1100

    Isolated the problem but haven't figured out the solution

--- software/cpp/2019.kuriosity/src/main/cpp/subsystems/SubPanelAffector.cpp
+++ software/cpp/2019.kuriosity/src/main/cpp/subsystems/SubPanelAffector.cpp
@@ -16,7 +16,7 @@ SubPanelAffector::SubPanelAffector() : Subsystem("SubPanelAffector") {
   solBottom = Robot::_robotMap->solPanelAffectorBottom;
   solTopFinger = Robot::_robotMap->solPanelAffectorTopFinger;
   solBottomFinger = Robot::_robotMap->solPanelAffectorBottomFinger;
-  //_controller = Robot::_oi->controller;
+  //_controller = Robot::_oi->controller; <-- this crashes the rio
   SmartDashboard::PutData("Pneumatic Test", new CmdOutputPanel(true));
 
   Retract();
@@ -48,10 +48,10 @@ void SubPanelAffector::RetractFingers() {
   solBottomFinger->Set(DoubleSolenoid::kReverse);
 }
 void SubPanelAffector::StartRumble() {
-  //_controller->SetRumble(GenericHID::kRightRumble, 1);
-  //_controller->SetRumble(GenericHID::kLeftRumble, 1);
+  _controller->SetRumble(GenericHID::kRightRumble, 1);
+  _controller->SetRumble(GenericHID::kLeftRumble, 1);
 }
 void SubPanelAffector::StopRumble() {
-  //_controller->SetRumble(GenericHID::kRightRumble, 0);
-  //_controller->SetRumble(GenericHID::kLeftRumble, 0);
+  _controller->SetRumble(GenericHID::kRightRumble, 0);
+  _controller->SetRumble(GenericHID::kLeftRumble, 0);
 }
