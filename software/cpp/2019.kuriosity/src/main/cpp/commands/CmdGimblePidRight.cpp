4f309a42437d4e8990351d95d0d1a24d95504b12
Darcy-Meago <darcymeago@gmail.com>
2019-02-01 14:20:04 +1100

    gimble: added all requires in PID commands for gimble

--- software/cpp/2019.kuriosity/src/main/cpp/commands/CmdGimblePidRight.cpp
+++ software/cpp/2019.kuriosity/src/main/cpp/commands/CmdGimblePidRight.cpp
@@ -1,15 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include "commands/CmdGimblePidRight.h"
+#include "Robot.h"
 
 CmdGimblePidRight::CmdGimblePidRight() {
-  // Use Requires() here to declare subsystem dependencies
-  // eg. Requires(Robot::chassis.get());
+  Requires(Robot::subGimble.get());
+  Requires(Robot::subGimbleLimits.get());
 }
 
 // Called just before this Command runs the first time
