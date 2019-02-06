3e1442dee819362608ae45e0c9f7906906b6f2f3
Darcy-Meago <darcymeago@gmail.com>
2019-02-06 21:50:13 +1100

    elevator: moved LimitButton due to misplace

--- software/cpp/2019.kuriosity/src/main/include/OI.h
+++ software/cpp/2019.kuriosity/src/main/include/OI.h
@@ -1,15 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #pragma once
 
 #include <iostream>
 #include <frc/WPILib.h>
 #include "AxisButton.h"
+#include "LimitButton.h"
 
 using namespace std;
 
