aeed7cff864c3b669673eaa9c5b770be819e6ab3
Mitch Blaser <mitchell.1080media@gmail.com>
2019-07-17 19:45:32 +1000

    Added USB Camera Server to Robot.cpp

--- software/cpp/AceDDU/src/main/cpp/Robot.cpp
+++ software/cpp/AceDDU/src/main/cpp/Robot.cpp
@@ -24,11 +24,12 @@ void Robot::RobotInit() {
   subDrivebase.reset(new SubDrivebase);
   subEncodedArm.reset(new SubEncodedArm);
   subClimber.reset(new SubClimber);
+  cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();
   std::cout << "SubFinished" << std::endl;
   m_oi.reset(new OI);
   std::cout << "m_oi Finished" << std::endl;
   autoRocket.reset(new Auto_rocket);
-  
+
   std::cout << "Init Finished" << std::endl;
 
   //Zero out/Initialize values on Shuffleboard
@@ -112,6 +113,8 @@ void Robot::TeleopInit() {
 
   frc::SmartDashboard::PutNumber("Arm Speed", 0);
 }
+
+
 
 void Robot::TeleopPeriodic() { frc::Scheduler::GetInstance()->Run(); }
 
