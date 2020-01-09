3a3fb2eefb7f39c6dd0539eea0ff5fc476018cba
Unknown <kikidunn2002@gmail.com>
2020-01-09 12:12:00 +1100

    Drivebase and drive command tested and working

--- software/cpp/2020.Roley/src/main/cpp/commands/CmdJoystickDrive.cpp
+++ software/cpp/2020.Roley/src/main/cpp/commands/CmdJoystickDrive.cpp
@@ -1,31 +1,37 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
-#include "commands/MyAutoCommand.h"
-
 #include "Robot.h"
+#include "commands/CmdJoystickDrive.h"
+#include <iostream>
 
-MyAutoCommand::MyAutoCommand() {
+CmdJoystickDrive::CmdJoystickDrive() {
+  Requires(Robot::subDriveBase.get());
   // Use Requires() here to declare subsystem dependencies
-  Requires(&Robot::m_subsystem);
+  // eg. Requires(Robot::chassis.get());
 }
 
 // Called just before this Command runs the first time
-void MyAutoCommand::Initialize() {}
+void CmdJoystickDrive::Initialize() {}
 
 // Called repeatedly when this Command is scheduled to run
-void MyAutoCommand::Execute() {}
+void CmdJoystickDrive::Execute() {
+  Robot::subDriveBase->drive(Robot::m_oi.getJoystickY(), Robot::m_oi.getJoystickX());
+}
 
 // Make this return true when this Command no longer needs to run execute()
-bool MyAutoCommand::IsFinished() { return false; }
+bool CmdJoystickDrive::IsFinished() { return false; }
 
 // Called once after isFinished returns true
-void MyAutoCommand::End() {}
+void CmdJoystickDrive::End() {
+  Robot::subDriveBase->drive(0,0);
+}
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void MyAutoCommand::Interrupted() {}
+void CmdJoystickDrive::Interrupted() {
+  End();
+}
--- software/cpp/2020.Roley/src/main/include/Robot.h
+++ software/cpp/2020.Roley/src/main/include/Robot.h
@@ -12,14 +12,12 @@
 #include <frc/smartdashboard/SendableChooser.h>
 
 #include "OI.h"
-#include "commands/ExampleCommand.h"
-#include "commands/MyAutoCommand.h"
-#include "subsystems/ExampleSubsystem.h"
+#include "subsystems/SubDriveBase.h"
 
 class Robot : public frc::TimedRobot {
  public:
-  static ExampleSubsystem m_subsystem;
   static OI m_oi;
+  static std::shared_ptr<SubDriveBase> subDriveBase;
 
   void RobotInit() override;
   void RobotPeriodic() override;
@@ -34,8 +32,8 @@ class Robot : public frc::TimedRobot {
  private:
   // Have it null by default so that if testing teleop it
   // doesn't have undefined behavior and potentially crash.
-  frc::Command* m_autonomousCommand = nullptr;
-  ExampleCommand m_defaultAuto;
-  MyAutoCommand m_myAuto;
-  frc::SendableChooser<frc::Command*> m_chooser;
+  //frc::Command* m_autonomousCommand = nullptr;
+  //ExampleCommand m_defaultAuto;
+  //MyAutoCommand m_myAuto;
+  //frc::SendableChooser<frc::Command*> m_chooser;
 };
