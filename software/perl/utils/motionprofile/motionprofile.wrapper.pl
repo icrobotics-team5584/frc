7419a8c237b7daff5cd9814b33a28ebb83dbfa2e
Ian Jessett <ian.jessett@versiontree.com>
2020-01-15 00:05:33 +1100

    Updated motionprofile utility to construct profile from segment definition in XML configuration file.

--- software/perl/utils/motionprofile/motionprofile.wrapper.pl
+++ software/perl/utils/motionprofile/motionprofile.wrapper.pl
@@ -12,46 +12,22 @@ use File::Copy;
 my $vscoderoot = "/c/scm/icrobotics/frc/software/cpp";
 my $vscodeprojectname = "CPP_MotionProfileArc_Simple";
 my $vscodeprojectpath = "src/main/include";
-my $deploy_single_header_file = 1;
-my $deploy_header_files = 0;
+my $deploy_header_files = 1;
 my $deploy_csv_files = 0;
 
 #####################################################################################
 
-print "INFO: creating profile for primary MotionProfile run\n";
+print "INFO: creating profiles using XML configuration\n";
 
-my $vprog = 5.00;
-my $dist = 40.00;
-my $curve2dist = 0.00;
-my $curve2offset = 0;
-
-runcmd( "perl motionprofile.pl -ident MotionProfile -mode SIMPLE -t1 400 -t2 200 -itp 10 -vprog ${vprog} -dist ${dist}" );
+runcmd( "perl motionprofile.pl" );
 
 print "INFO: creating graphs for MotionProfile run\n";
 
-runcmd( "perl motionprofile.graph.pl -ident MotionProfile" );
-
-#####################################################################################
+runcmd( "perl motionprofile.graph.pl" );
 
 print "INFO: creating html document for all profiles\n";
 
 runcmd( "perl motionprofile.document.pl" );
-
-#####################################################################################
-
-if( $deploy_single_header_file )
-  {
-  my $targetdir = "${vscoderoot}/${vscodeprojectname}/${vscodeprojectpath}";
-  if( -d $targetdir )
-    {
-    print "INFO: transfering MotionProfile.h files to vscode project $vscodeprojectname (please refresh project to reload)\n";
-    copy( "MotionProfileMotionProfile.h", "$targetdir/MotionProfile.h" );
-    }
-  else
-    {
-    print "WARNING: unable to transfer *.h files to vscode project $vscodeprojectname - target directory does not exist!\n";
-    }
-  }
 
 #####################################################################################
 
@@ -100,3 +76,5 @@ sub runcmd
   my ( $cmd ) = @_;
   print `$cmd 2>&1`;
   }
+
+#####################################################################################
