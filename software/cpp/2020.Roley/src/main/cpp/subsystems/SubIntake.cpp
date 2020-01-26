a9a140122e216fa6c346c5a49a54ef1f6999b58a
Darcy-Meago <darcymeago@gmail.com>
2020-01-26 13:47:09 +1100

    2020.Roley_Janitor changed current limiting for intake to false untill updated

--- software/cpp/2020.Roley/src/main/cpp/subsystems/SubIntake.cpp
+++ software/cpp/2020.Roley/src/main/cpp/subsystems/SubIntake.cpp
@@ -30,7 +30,7 @@ SubIntake::SubIntake() : Subsystem("ExampleSubsystem") {
   srxIntake->ConfigPeakCurrentLimit(0); //(current limit in amps(integer), timeout in milliseconds)
 	srxIntake->ConfigPeakCurrentDuration(0); //(time over limit to trigger in milliseconds, timeout in milliseconds)
 	srxIntake->ConfigContinuousCurrentLimit(1); //(amps(integer) to set current to, timeout in milliseconds)
-	srxIntake->EnableCurrentLimit(true);
+	srxIntake->EnableCurrentLimit(false);
 
   frc::SmartDashboard::PutNumber("Intake Speed", kDefaultSpeed);
 }
