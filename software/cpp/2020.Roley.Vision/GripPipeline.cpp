e86868b65d41f9c8629026a9e5d09f00d95ec0c0
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2020-01-27 23:59:33 +1100

    Added new 2020.Roley.Vision CPP project with content from JETSON NANO (201-ic-pipeline project - OPENCV on CPU) plus remote build capability.

---
+++ software/cpp/2020.Roley.Vision/GripPipeline.cpp
@@ -0,0 +1,203 @@
+#include "GripPipeline.h"
+
+namespace grip {
+
+GripPipeline::GripPipeline() {
+}
+/**
+* Runs an iteration of the pipeline and updates outputs.
+*/
+void GripPipeline::Process(cv::Mat& source0){
+	//Step Resize_Image0:
+	//input
+	cv::Mat resizeImageInput = source0;
+	double resizeImageWidth = 320.0;  // default Double
+	double resizeImageHeight = 180.0;  // default Double
+	int resizeImageInterpolation = cv::INTER_CUBIC;
+	resizeImage(resizeImageInput, resizeImageWidth, resizeImageHeight, resizeImageInterpolation, this->resizeImageOutput);
+	//Step Blur0:
+	//input
+	cv::Mat blurInput = resizeImageOutput;
+	BlurType blurType = BlurType::GAUSSIAN;
+	double blurRadius = 4.118404732094154;  // default Double
+	blur(blurInput, blurType, blurRadius, this->blurOutput);
+	//Step HSV_Threshold0:
+	//input
+	cv::Mat hsvThresholdInput = blurOutput;
+	double hsvThresholdHue[] = {60, 92};
+	double hsvThresholdSaturation[] = {100, 255};
+	double hsvThresholdValue[] = {72, 231};
+	hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, this->hsvThresholdOutput);
+	//Step Find_Contours0:
+	//input
+	cv::Mat findContoursInput = hsvThresholdOutput;
+	bool findContoursExternalOnly = false;  // default Boolean
+	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
+	//Step Filter_Contours0:
+	//input
+	std::vector<std::vector<cv::Point> > filterContoursContours = findContoursOutput;
+	double filterContoursMinArea = 200.0;  // default Double
+	double filterContoursMinPerimeter = 0.0;  // default Double
+	double filterContoursMinWidth = 0.0;  // default Double
+	double filterContoursMaxWidth = 1000.0;  // default Double
+	double filterContoursMinHeight = 0.0;  // default Double
+	double filterContoursMaxHeight = 1000.0;  // default Double
+	double filterContoursSolidity[] = {0, 100};
+	double filterContoursMaxVertices = 1000000.0;  // default Double
+	double filterContoursMinVertices = 0.0;  // default Double
+	double filterContoursMinRatio = 0.0;  // default Double
+	double filterContoursMaxRatio = 1000.0;  // default Double
+	filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, this->filterContoursOutput);
+}
+
+void GripPipeline::setsource0(cv::Mat &source0){
+	source0.copyTo(this->source0);
+}
+
+/**
+ * This method is a generated getter for the output of a Resize_Image.
+ * @return Mat output from Resize_Image.
+ */
+cv::Mat* GripPipeline::GetResizeImageOutput(){
+	return &(this->resizeImageOutput);
+}
+/**
+ * This method is a generated getter for the output of a Blur.
+ * @return Mat output from Blur.
+ */
+cv::Mat* GripPipeline::GetBlurOutput(){
+	return &(this->blurOutput);
+}
+/**
+ * This method is a generated getter for the output of a HSV_Threshold.
+ * @return Mat output from HSV_Threshold.
+ */
+cv::Mat* GripPipeline::GetHsvThresholdOutput(){
+	return &(this->hsvThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a Find_Contours.
+ * @return ContoursReport output from Find_Contours.
+ */
+std::vector<std::vector<cv::Point> >* GripPipeline::GetFindContoursOutput(){
+	return &(this->findContoursOutput);
+}
+/**
+ * This method is a generated getter for the output of a Filter_Contours.
+ * @return ContoursReport output from Filter_Contours.
+ */
+std::vector<std::vector<cv::Point> >* GripPipeline::GetFilterContoursOutput(){
+	return &(this->filterContoursOutput);
+}
+	/**
+	 * Scales and image to an exact size.
+	 *
+	 * @param input The image on which to perform the Resize.
+	 * @param width The width of the output in pixels.
+	 * @param height The height of the output in pixels.
+	 * @param interpolation The type of interpolation.
+	 * @param output The image in which to store the output.
+	 */
+	void GripPipeline::resizeImage(cv::Mat &input, double width, double height, int interpolation, cv::Mat &output) {
+		cv::resize(input, output, cv::Size(width, height), 0.0, 0.0, interpolation);
+	}
+
+	/**
+	 * Softens an image using one of several filters.
+	 *
+	 * @param input The image on which to perform the blur.
+	 * @param type The blurType to perform.
+	 * @param doubleRadius The radius for the blur.
+	 * @param output The image in which to store the output.
+	 */
+	void GripPipeline::blur(cv::Mat &input, BlurType &type, double doubleRadius, cv::Mat &output) {
+		int radius = (int)(doubleRadius + 0.5);
+		int kernelSize;
+		switch(type) {
+			case BOX:
+				kernelSize = 2 * radius + 1;
+				cv::blur(input,output,cv::Size(kernelSize, kernelSize));
+				break;
+			case GAUSSIAN:
+				kernelSize = 6 * radius + 1;
+				cv::GaussianBlur(input, output, cv::Size(kernelSize, kernelSize), radius);
+				break;
+			case MEDIAN:
+				kernelSize = 2 * radius + 1;
+				cv::medianBlur(input, output, kernelSize);
+				break;
+			case BILATERAL:
+				cv::bilateralFilter(input, output, -1, radius, radius);
+				break;
+        }
+	}
+	/**
+	 * Segment an image based on hue, saturation, and value ranges.
+	 *
+	 * @param input The image on which to perform the HSL threshold.
+	 * @param hue The min and max hue.
+	 * @param sat The min and max saturation.
+	 * @param val The min and max value.
+	 * @param output The image in which to store the output.
+	 */
+	void GripPipeline::hsvThreshold(cv::Mat &input, double hue[], double sat[], double val[], cv::Mat &out) {
+		cv::cvtColor(input, out, cv::COLOR_BGR2HSV);
+		cv::inRange(out,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), out);
+	}
+
+	/**
+	 * Finds contours in an image.
+	 *
+	 * @param input The image to find contours in.
+	 * @param externalOnly if only external contours are to be found.
+	 * @param contours vector of contours to put contours in.
+	 */
+	void GripPipeline::findContours(cv::Mat &input, bool externalOnly, std::vector<std::vector<cv::Point> > &contours) {
+		std::vector<cv::Vec4i> hierarchy;
+		contours.clear();
+		int mode = externalOnly ? cv::RETR_EXTERNAL : cv::RETR_LIST;
+		int method = cv::CHAIN_APPROX_SIMPLE;
+		cv::findContours(input, contours, hierarchy, mode, method);
+	}
+
+
+	/**
+	 * Filters through contours.
+	 * @param inputContours is the input vector of contours.
+	 * @param minArea is the minimum area of a contour that will be kept.
+	 * @param minPerimeter is the minimum perimeter of a contour that will be kept.
+	 * @param minWidth minimum width of a contour.
+	 * @param maxWidth maximum width.
+	 * @param minHeight minimum height.
+	 * @param maxHeight  maximimum height.
+	 * @param solidity the minimum and maximum solidity of a contour.
+	 * @param minVertexCount minimum vertex Count of the contours.
+	 * @param maxVertexCount maximum vertex Count.
+	 * @param minRatio minimum ratio of width to height.
+	 * @param maxRatio maximum ratio of width to height.
+	 * @param output vector of filtered contours.
+	 */
+	void GripPipeline::filterContours(std::vector<std::vector<cv::Point> > &inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double solidity[], double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, std::vector<std::vector<cv::Point> > &output) {
+		std::vector<cv::Point> hull;
+		output.clear();
+		for (std::vector<cv::Point> contour: inputContours) {
+			cv::Rect bb = boundingRect(contour);
+			if (bb.width < minWidth || bb.width > maxWidth) continue;
+			if (bb.height < minHeight || bb.height > maxHeight) continue;
+			double area = cv::contourArea(contour);
+			if (area < minArea) continue;
+			if (arcLength(contour, true) < minPerimeter) continue;
+			cv::convexHull(cv::Mat(contour, true), hull);
+			double solid = 100 * area / cv::contourArea(hull);
+			if (solid < solidity[0] || solid > solidity[1]) continue;
+			if (contour.size() < minVertexCount || contour.size() > maxVertexCount)	continue;
+			double ratio = (double) bb.width / (double) bb.height;
+			if (ratio < minRatio || ratio > maxRatio) continue;
+			output.push_back(contour);
+		}
+	}
+
+
+
+} // end grip namespace
+
---
+++ software/cpp/2020.Roley.Vision/ic_pipeline.cpp
@@ -0,0 +1,212 @@
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "GripPipeline.h"
+#include <ctime>
+#include <iostream>
+#include <fstream>
+
+#include <chrono>
+#include <cstdio>
+#include <thread>
+#include "ntcore.h"
+
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableInstance.h"
+
+using namespace cv;
+using namespace std;
+// using namespace cv::gpu;
+
+
+RNG rng(12345);
+int peg_hits = 0;
+int peg_misses = 0;
+int debug = 0;
+
+int main( int argc, char *argv[] )
+{
+
+  // handle command line arguments
+  if( ( argc > 1 ) && ( strcmp( argv[1], "--debug" ) == 0 ) )
+    debug = 1;
+
+  // setup image pipeline
+  cv::Mat img;
+  //cv::GpuMat g_img(img);
+  grip::GripPipeline ic_pipeline;
+  cv::VideoCapture input(0);
+
+  // record start time
+  clock_t start = clock();
+
+  // setup network tables connection
+  nt::NetworkTableInstance ntinst = nt::NetworkTableInstance::GetDefault();
+  std::shared_ptr<nt::NetworkTable> nt;
+  nt = ntinst.GetTable("JETSON");
+  ntinst.StartClientTeam(5584);
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+
+  for (;;)
+  {
+
+    // STEP 1: fetch image
+    if(!input.read(img))
+      break;
+
+    // STEP 2: setup image pipeline
+    ic_pipeline.setsource0(img);
+    ic_pipeline.Process(img);
+
+    // STEP 3: obtain intermediate images and countour vectors
+    cv::Mat* img_hsvthreshold = ic_pipeline.GetHsvThresholdOutput();
+    cv::Mat* img_blur = ic_pipeline.GetBlurOutput();
+    // std::vector<std::vector<cv::Point> >* img_findcontours = ic_pipeline.GetFindContoursOutput();
+    std::vector<std::vector<cv::Point> >* img_filtercontours = ic_pipeline.GetFilterContoursOutput();
+
+    // STEP 4: output contour x, y, width and height
+    cout << "INFO: bounding rectangles for each contour: (x,y):(width,height)" << endl;
+    for (std::vector<cv::Point> contour: *img_filtercontours)
+    {
+      cv::Rect br = boundingRect(contour);
+      cout << "(" << br.x << "," << br.y << "):(" << br.width << "," << br.height << ")" << endl;
+    }
+
+    // STEP 5: construct image to display filetered contours, bounding rectangles and origins of rectangles
+    cv::Mat img_contours = cv::Mat::zeros( img.size(), CV_8UC3 );
+    int img_width = 320;  //img.size().width;
+    int img_height = 180; //img.size().height;
+    for( size_t i = 0; i< img_filtercontours->size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       drawContours( img_contours, *img_filtercontours, (int)i, color, 2, 8 );
+     }
+    int contourcount = 0;
+    int min_x = 1000000;
+    int max_x = 0;
+    int min_y = 1000000;
+    int max_y = 0;
+    double peg_x = 0.0;
+    double peg_y = 0.0;
+    double peg_range = 0.0;
+    double bounding_rectangle_width = 0.0;
+    double bounding_rectangle_height = 0.0;
+    for (std::vector<cv::Point> contour: *img_filtercontours)
+    {
+      Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+      cv::Rect br = boundingRect(contour);
+      cv::rectangle( img_contours, br, color, 2, 8 );
+      Point origin;
+      origin.x = br.x;
+      origin.y = br.y;
+      cv::circle( img_contours, origin, 5, color, 2, 8 );
+      int topleft_x = br.x;
+      int topleft_y = br.y;
+      int bottomright_x = br.x + br.width;
+      int bottomright_y = br.y + br.height;
+      if( topleft_x < min_x ) { min_x = topleft_x; }
+      if( topleft_y < min_y ) { min_y = topleft_y; }
+      if( bottomright_x > max_x ) { max_x = bottomright_x; }
+      if( bottomright_y > max_y ) { max_y = bottomright_y; }
+      contourcount++;
+    }
+    int status;
+    cout << "INFO: number of contours detected: " << contourcount << endl;
+    if( contourcount == 1 )
+    {
+      status = 0;
+      peg_hits++;
+      double x = ( min_x + max_x ) / 2;
+      double y = min_y;
+      cout << "INFO: estimated peg position in image: (x,y)" << endl;
+      cout << "(" << x << "," << y << ")" << endl;
+      bounding_rectangle_width = 1.0 * (max_x - min_x) / img_width;
+      bounding_rectangle_height = 1.0 * (max_y - min_y) / img_height;
+      cout << "INFO: bounding rectangle size as proportion of FOV: (width,height)" << endl;
+      cout << "(" << bounding_rectangle_width << "," << bounding_rectangle_height << ")" << endl;
+      // calculate peg postion with image center (0,0) and bottom left (-1,-1) and top right (+1,+1)
+      peg_x = 30 * ( x - ( img_width / 2 ) ) / ( img_width / 2 );
+      peg_y = -17 * ( y - ( img_height / 2 ) ) / ( img_height / 2 );
+      cout << "INFO: estimated peg position in field of view: (x,y)" << endl;
+      cout << "(" << peg_x << "," << peg_y << ")" << endl;
+      // estimate range based on prior knowledge of camera vertical FOV and target height assume 45 degree (0.785 radian) vertical FOV and 200mm target height
+      peg_range = 200 / tan( 0.785 * ( max_y - min_y ) / img_height );
+      cout << "INFO: estimated peg range: (mm)" << endl;
+      cout << peg_range << "mm" << endl;
+      // display peg position at x, y on contours image ...
+      Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+      Point peg;
+      peg.x = x;
+      peg.y = y;
+      cv::circle( img_contours, peg, 10, color, 2, 8 );
+    }
+    else
+    {
+      status = 1;
+      peg_misses++;
+      peg_x =0.0; 
+      peg_y = 0.0;
+      peg_range = 0.0;
+      bounding_rectangle_width = 0.0;
+      bounding_rectangle_height = 0.0;
+      cout << "INFO: unable to detect peg position" << endl;
+    }
+    double peg_hitrate = ( 100.0 * peg_hits ) / ( peg_misses + peg_hits );
+    cout << "INFO: peg position detections so far: " << peg_hits << " (" << peg_misses << ") (" << peg_hitrate << "% hitrate)" << endl;
+    clock_t now = clock();
+    double elapsedsecs = ( now - start ) / (double) CLOCKS_PER_SEC;
+    double fps = (double) 1.0 / elapsedsecs;
+    start = now;
+    cout << "INFO: elapsed seconds since last frame: " << elapsedsecs << endl;
+    cout << "INFO: frames per second: " << fps << endl;
+
+    // STEP 6: display images
+    if( debug == 1 )
+    {
+      cv::imshow( "img", img );
+      cv::imshow( "hsv threshold", *img_hsvthreshold );
+      cv::imshow( "blur", *img_blur );
+      cv::imshow( "img_contours", img_contours );
+    }
+
+    // STEP 7: update network tables
+    cout << "INFO: updating network tables" << endl;
+    nt->PutNumber( "status", status );
+    nt->PutNumber( "timestamp", (int)now );
+    nt->PutNumber( "pegx", peg_x );
+    nt->PutNumber( "pegy", peg_y );
+    nt->PutNumber( "pegrange", peg_range );
+    nt->PutNumber( "boundingrectangleheight", bounding_rectangle_height );
+    nt->PutNumber( "boundingrectanglewidth", bounding_rectangle_width );
+    nt->PutNumber( "hitrate", peg_hitrate );
+    nt->PutNumber( "elapsedsecs", elapsedsecs );
+    nt->PutNumber( "fps", fps );
+    cout << "-----" << endl;
+
+    // STEP 8: check for control file
+    string line;
+    ifstream ctlfile1("ic_pipeline.stop");
+    ifstream ctlfile2("/tmp/ic_pipeline.stop");
+    int running = 1; 
+    if( ctlfile1.is_open() || ctlfile2.is_open() )
+    {
+      running = 0;
+      ctlfile1.close();
+      ctlfile2.close();
+    }
+    if( running == 0 )
+    {
+      cout << "INFO: detected control file (stop)" << endl;
+      break;
+    }
+
+    // STEP 9: check for user request to terminate
+    char c = cv::waitKey(30);
+    if ( c == ' ' )
+      break;
+
+  }
+
+}
+
+
