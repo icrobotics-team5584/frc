1def8989f96256630189fe0203adcf81496e9597
Kiki <kikidunn2002@gmail.com>
2020-02-07 15:17:19 +1100

    Merge conflicts, integration and control fixed

--- software/cpp/2020.Roley/src/main/cpp/OI.cpp
+++ software/cpp/2020.Roley/src/main/cpp/OI.cpp
@@ -51,13 +51,13 @@ OI::OI() {
 
   //Climber Button
 
-  btnClimb.reset(new frc::JoystickButton(joystick1.get(), leftStickBtn));
-  btnClimb->WhileHeld(new CmdClimberLimitUp());
-  btnRatchets.reset(new frc::JoystickButton(joystick1.get(), aBtn));
-  btnRatchets->WhileHeld(new CmdEngageClimberRatchets());
+  //btnClimb.reset(new frc::JoystickButton(joystick1.get(), leftStickBtn));
+  //btnClimb->WhileHeld(new CmdClimberLimitUp());
+  //btnRatchets.reset(new frc::JoystickButton(joystick1.get(), aBtn));
+  //btnRatchets->WhileHeld(new CmdEngageClimberRatchets());
 
   //Storage Pneumatics
-  btnStorageExpand.reset(new frc::JoystickButton(joystick1.get(),aBtn));
+  btnStorageExpand.reset(new frc::JoystickButton(joystick1.get(), aBtn));
   btnStorageExpand->ToggleWhenPressed(new CmdStorageTogglePneumatic);
 
   //std::cout<< "OI Ended" << std::endl;
--- software/cpp/2020.Roley/src/main/cpp/Robot.cpp
+++ software/cpp/2020.Roley/src/main/cpp/Robot.cpp
@@ -102,6 +102,7 @@ void Robot::AutonomousPeriodic() {
 
 void Robot::TeleopInit() {
   subDriveBase->retractDolly();
+  subClimber->RatchetsDisengage();
   // This makes sure that the autonomous stops running when
   // teleop starts running. If you want the autonomous to
   // continue until interrupted by another command, remove
