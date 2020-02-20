088f622b0e97ff0b57b4d60500023e2a615472eb
Kiki <kikidunn2002@gmail.com>
2020-02-20 15:55:00 +1100

    Auto command fixes

--- software/cpp/2020.Roley/src/main/cpp/commands/CmdAutoIntake.cpp
+++ software/cpp/2020.Roley/src/main/cpp/commands/CmdAutoIntake.cpp
@@ -27,7 +27,7 @@ void CmdAutoIntake::Initialize() {
 void CmdAutoIntake::Execute() {}
 
 // Make this return true when this Command no longer needs to run execute()
-bool CmdAutoIntake::IsFinished() { return true; }
+bool CmdAutoIntake::IsFinished() { return false; }
 
 // Called once after isFinished returns true
 void CmdAutoIntake::End() {
