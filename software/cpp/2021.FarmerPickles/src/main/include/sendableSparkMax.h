2f78cd5429feb47cdfb19bfe3355cfdb318e06d5
Mitch Blaser <mitchell.1080media@gmail.com>
2021-01-13 17:58:27 +1100

    WIP: SendableSparkMax

---
+++ software/cpp/2021.FarmerPickles/src/main/include/sendableSparkMax.h
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <rev/CANSparkMax.h>
+#include <frc/smartdashboard/Sendable.h>
+#include <frc/smartdashboard/SendableBuilder.h>
+#include <frc/smartdashboard/SendableBuilderImpl.h>
+
+class sendableSparkMax :
+public rev::CANSparkMax,
+public frc::Sendable
+{
+ public:
+  sendableSparkMax(int deviceID, MotorType type);
+
+ private:
+  void InitSendable(frc::SendableBuilder& builder);
+};
