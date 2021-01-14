b4d15852af0678859d4cfa69c1b66e41d3713c0f
Alex Lai <aexace666@gmail.com>
2021-01-14 19:14:44 +1100

    returned null for auto function to fix warning

--- software/cpp/2021.RoleyToo/src/main/cpp/RobotContainer.cpp
+++ software/cpp/2021.RoleyToo/src/main/cpp/RobotContainer.cpp
@@ -17,5 +17,5 @@ void RobotContainer::ConfigureButtonBindings() {
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
   // An example command will be run in autonomous
-  //return;
+  return NULL;
 }
