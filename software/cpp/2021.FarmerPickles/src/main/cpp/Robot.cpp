cbca09af91e8a5f589d40aa31846873816acfcd9
Dhiluka Mapa <dhiluka.mapa@hotmail.com>
2021-01-18 21:02:26 +1100

    Added Auto Limelight LED Toggle
    Limelight turns on when command is activated and turns off when command ends

--- software/cpp/2021.FarmerPickles/src/main/cpp/Robot.cpp
+++ software/cpp/2021.FarmerPickles/src/main/cpp/Robot.cpp
@@ -7,7 +7,9 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
 
-void Robot::RobotInit() { std::cout << "It's ALIVE!!!! \n"; }
+void Robot::RobotInit() {
+  std::cout << "It's ALIVE!!!! \n";
+}
 
 /**
  * This function is called every robot packet, no matter the mode. Use
