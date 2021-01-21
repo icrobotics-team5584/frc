bb7eb16cdfe944edadb06be040d2b247ff0ec5cc
Alex Lai <aexace666@gmail.com>
2021-01-21 19:25:02 +1100

    added code for color sensor, able to identify the four colors and print it on the shuffleboard

--- software/cpp/2021.RoleyToo/src/main/cpp/subsystems/SubColorWheel.cpp
+++ software/cpp/2021.RoleyToo/src/main/cpp/subsystems/SubColorWheel.cpp
@@ -4,13 +4,33 @@
 
 #include "subsystems/SubColorWheel.h"
 #include <frc/smartdashboard/smartdashboard.h>
+#include <array>
+#include <iostream>
 
 SubColorWheel::SubColorWheel() = default;
 
 // This method will be called once per scheduler run
 void SubColorWheel::Periodic() {
     frc::Color detectedColor = _colorSensor.GetColor();
-    frc::SmartDashboard::PutNumber("Red", detectedColor.red);
-    frc::SmartDashboard::PutNumber("Green", detectedColor.green);
-    frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
+    double _red = detectedColor.red;
+    double _green = detectedColor.green;
+    double _blue = detectedColor.blue;
+
+    std::array<Color, 4> colorArray {red, green, blue, yellow}; 
+    for(Color currentColor : colorArray){
+        double currentError = currentColor.GetError(_red, _green, _blue);
+        if(smallestError > currentError){
+            smallestError = currentError;
+            smallestColor = currentColor;
+        } 
+    }
+
+    frc::SmartDashboard::PutString("Color", smallestColor._name);
+    
+    smallestError = 1;
+    smallestColor = Color{0, 0, 0, "None"};
+
+    // frc::SmartDashboard::PutNumber("Red", detectedColor.red);
+    // frc::SmartDashboard::PutNumber("Green", detectedColor.green);
+    // frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
 }
--- software/cpp/2021.RoleyToo/src/main/include/subsystems/SubColorWheel.h
+++ software/cpp/2021.RoleyToo/src/main/include/subsystems/SubColorWheel.h
@@ -6,6 +6,39 @@
 
 #include <frc2/command/SubsystemBase.h>
 #include <rev/ColorSensorV3.h>
+#include <map>
+#include <string>
+#include <stdlib.h>
+
+struct Color{
+  Color(double red, double green, double blue, std::string name){
+    _red = red;
+    _blue = blue;
+    _green = green;
+    _name = name;
+  }
+  double GetError(double detectedRed, double detectedGreen, double detectedBlue){
+    double sumError = 0;
+    // std::cout << _name << sumError;
+    // if(detectedRed > _red){sumError += (detectedRed - _red);}
+    // else{sumError += (_red - detectedRed);}
+    // std::cout << sumError;
+    // if(detectedGreen > _green){sumError += (detectedGreen - _green);}
+    // else{sumError += (_green - detectedGreen);}
+    // std::cout << sumError;
+    // if(detectedBlue > _blue){sumError += (detectedBlue - _blue);}
+    // else{sumError += (_blue - detectedBlue);}
+    // std::cout << sumError << std::endl;
+    sumError += abs(detectedRed - _red);
+    sumError += abs(detectedGreen - _green);
+    sumError += abs(detectedBlue - _blue);
+    return sumError;
+  }
+  double _red;
+  double _blue;
+  double _green;
+  std::string _name; 
+};
 
 class SubColorWheel : public frc2::SubsystemBase {
  public:
@@ -18,6 +51,15 @@ class SubColorWheel : public frc2::SubsystemBase {
 
  private:
   rev::ColorSensorV3 _colorSensor{frc::I2C::Port::kOnboard};
+
+  Color blue{0.14, 0.44, 0.42, "Blue"};
+  Color green{0.18, 0.565, 0.253, "Green"};
+  Color red{0.463, 0.38, 0.156, "Red"};
+  Color yellow{0.31, 0.557, 0.133, "Yellow"};
+  
+
+  double smallestError = 1;
+  Color smallestColor{0, 0, 0, "None"};
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
