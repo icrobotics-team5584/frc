9cd956099c02abd3e74334f4c358d2d8cd701ebe
Kiki Dunn <kikidunn2002@gmail.com>
2021-04-13 14:15:03 +1000

    auto testing

--- software/cpp/2021.RoleyToo/src/main/cpp/commands/CmdAuto10Ball.cpp
+++ software/cpp/2021.RoleyToo/src/main/cpp/commands/CmdAuto10Ball.cpp
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "commands/CmdAuto10Ball.h"
-
+#include <frc2/command/InstantCommand.h>
 // NOTE:  Consider using this command inline, rather than writing a subclass.
 // For more information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
@@ -11,9 +11,13 @@ CmdAuto10Ball::CmdAuto10Ball(SubDriveBase* subDriveBase, SubIntake* subIntake) :
   [subDriveBase]{return subDriveBase->getYaw();}, 
   [subDriveBase]{return subDriveBase->getDistanceTravelled();}
 } {
-  AddCommands(CmdAutoSetPose{&_autonomous, -1.6, 3.548,  0},
+  AddCommands(CmdAutoSetPose{&_autonomous, -1.7, 3.548,  0},
               //                                    startx, starty, endx, endy, endheading, cenx, ceny
-              CmdAutoDrive{subDriveBase, &_autonomous, -1.7, 4.048, -1.7, 9.514, 0, 0, 0, PIDk{-5, 0, -50}, 1, 0, PIDk{1, 0, 0}}
+              CmdAutoIntakeDeploy(subIntake),
+              frc2::InstantCommand([subIntake]{subIntake->Intake(1);}),
+              CmdAutoDrive{subDriveBase, &_autonomous, -1.7, 4.048, -1.7, 9.514, 0, 0, 0, PIDk{-5, 0, -50}, 0.75, 0, PIDk{1, 0, 0}},
+              frc2::InstantCommand([subIntake]{subIntake->Stop();}),
+              CmdAutoIntakeRetract(subIntake)
               //CmdAutoDrive{subDriveBase, &_autonomous, -1.7, 7.048, -1.7, 4.048, 0, 0, 0, PIDk{-1, 0, -20}, -1, 0, PIDk{-0.6, 0, 0}}
               //CmdAutoTurn{subDriveBase, &_autonomous, PIDk{1, 0, 20}, 180, 10}
               //CmdAutoDrive{subDriveBase, &_autonomous, 6.548, -1.7, 3.348,-1.7, 180, 0, 0, PIDk{3.2, 0, 20}, 0.1}
