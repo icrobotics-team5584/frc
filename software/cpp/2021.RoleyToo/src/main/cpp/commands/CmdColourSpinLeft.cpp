c077f48045ddb302698427ae77436d0a3ee27dcb
Kiki Dunn <kikidunn2002@gmail.com>
2021-04-21 21:31:29 +1000

    starting colour wheel updates for current design

--- software/cpp/2021.RoleyToo/src/main/cpp/commands/CmdColourSpinLeft.cpp
+++ software/cpp/2021.RoleyToo/src/main/cpp/commands/CmdColourSpinLeft.cpp
@@ -2,26 +2,25 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/CmdDeployColorwheel.h"
+#include "commands/CmdColourSpinLeft.h"
 
-CmdDeployColorwheel::CmdDeployColorwheel(SubColorWheel* subColorWheel) {
+CmdColourSpinLeft::CmdColourSpinLeft(SubColorWheel* subColorWheel) {
   // Use addRequirements() here to declare subsystem dependencies.
   AddRequirements(subColorWheel);
   _subColorWheel = subColorWheel;
 }
 
+
 // Called when the command is initially scheduled.
-void CmdDeployColorwheel::Initialize() {
-  _subColorWheel->DeployColorWheel();
-}
+void CmdColourSpinLeft::Initialize() {}
 
 // Called repeatedly when this Command is scheduled to run
-void CmdDeployColorwheel::Execute() {}
+void CmdColourSpinLeft::Execute() {}
 
 // Called once the command ends or is interrupted.
-void CmdDeployColorwheel::End(bool interrupted) {}
+void CmdColourSpinLeft::End(bool interrupted) {}
 
 // Returns true when the command should end.
-bool CmdDeployColorwheel::IsFinished() {
-  return true;
+bool CmdColourSpinLeft::IsFinished() {
+  return false;
 }
--- software/cpp/2021.RoleyToo/src/main/cpp/commands/CmdColourSpinRight.cpp
+++ software/cpp/2021.RoleyToo/src/main/cpp/commands/CmdColourSpinRight.cpp
@@ -2,26 +2,25 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/CmdDeployColorwheel.h"
+#include "commands/CmdColourSpinRight.h"
 
-CmdDeployColorwheel::CmdDeployColorwheel(SubColorWheel* subColorWheel) {
+CmdColourSpinRight::CmdColourSpinRight(SubColorWheel* subColorWheel) {
   // Use addRequirements() here to declare subsystem dependencies.
   AddRequirements(subColorWheel);
   _subColorWheel = subColorWheel;
 }
 
+
 // Called when the command is initially scheduled.
-void CmdDeployColorwheel::Initialize() {
-  _subColorWheel->DeployColorWheel();
-}
+void CmdColourSpinRight::Initialize() {}
 
 // Called repeatedly when this Command is scheduled to run
-void CmdDeployColorwheel::Execute() {}
+void CmdColourSpinRight::Execute() {}
 
 // Called once the command ends or is interrupted.
-void CmdDeployColorwheel::End(bool interrupted) {}
+void CmdColourSpinRight::End(bool interrupted) {}
 
 // Returns true when the command should end.
-bool CmdDeployColorwheel::IsFinished() {
-  return true;
+bool CmdColourSpinRight::IsFinished() {
+  return false;
 }
