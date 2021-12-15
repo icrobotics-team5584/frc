f2b2ed2663bfa29ec9aed13e2630f5954935a635
Aaron Kennedy <aaronkennedy1000@gmail.com>
2021-12-15 21:20:02 +1100

    made new project for practice bot

---
+++ software/cpp/2021.Prerobot/Prerobot/src/main/cpp/subsystems/SubDrivebase.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/SubDrivebase.h"
+
+SubDrivebase::SubDrivebase()
+{
+    backLeft.Follow(frontLeft);
+    backRight.Follow(frontRight);
+};
+
+// This method will be called once per scheduler run
+void SubDrivebase::Periodic() {}
+void SubDrivebase::Drive(double speed, double steering)
+{
+    Diffdrive.ArcadeDrive(speed, steering);
+}
+
---
+++ software/cpp/2021.Prerobot/Prerobot/src/main/include/subsystems/SubDrivebase.h
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <rev/CANSparkMax.h>
+#include <frc/drive/DifferentialDrive.h>
+
+class SubDrivebase : public frc2::SubsystemBase {
+ public:
+  SubDrivebase();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  void Drive(double speed, double steering);
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+  rev::CANSparkMax frontLeft{4,  rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+  rev::CANSparkMax frontRight{3, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+  rev::CANSparkMax backLeft{2, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+  rev::CANSparkMax backRight{1, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+  frc::DifferentialDrive Diffdrive{frontLeft, frontRight};
+};
