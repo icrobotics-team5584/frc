aa11ab49da31cbf6140a7a4c2bc0b1565d3187d8
Aaron Kennedy <aaronkennedy1000@gmail.com>
2022-01-06 13:30:54 +1100

    Created deploy dolly command and retract dolly command

---
+++ software/cpp/2021.Prerobot/Prerobot/src/main/cpp/commands/CmdDeployDolly.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/CmdDeployDolly.h"
+
+CmdDeployDolly::CmdDeployDolly() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void CmdDeployDolly::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void CmdDeployDolly::Execute() {}
+
+// Called once the command ends or is interrupted.
+void CmdDeployDolly::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool CmdDeployDolly::IsFinished() {
+  return false;
+}
---
+++ software/cpp/2021.Prerobot/Prerobot/src/main/cpp/commands/CmdRetractDolly.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/CmdRetractDolly.h"
+
+CmdRetractDolly::CmdRetractDolly() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void CmdRetractDolly::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void CmdRetractDolly::Execute() {}
+
+// Called once the command ends or is interrupted.
+void CmdRetractDolly::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool CmdRetractDolly::IsFinished() {
+  return false;
+}
