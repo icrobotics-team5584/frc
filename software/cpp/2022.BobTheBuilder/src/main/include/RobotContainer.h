cb22bd9a56f461e4180cad0a909b5fdccfbc2bd4
Ace5584 <aexace666@gmail.com>
2022-01-10 16:10:10 +1100

    Delete Example subsystem and command

--- software/cpp/2022.BobTheBuilder/src/main/include/RobotContainer.h
+++ software/cpp/2022.BobTheBuilder/src/main/include/RobotContainer.h
@@ -6,9 +6,6 @@
 
 #include <frc2/command/Command.h>
 
-#include "commands/ExampleCommand.h"
-#include "subsystems/ExampleSubsystem.h"
-
 /**
  * This class is where the bulk of the robot should be declared.  Since
  * Command-based is a "declarative" paradigm, very little robot logic should
@@ -23,9 +20,6 @@ class RobotContainer {
   frc2::Command* GetAutonomousCommand();
 
  private:
-  // The robot's subsystems and commands are defined here...
-  ExampleSubsystem m_subsystem;
-  ExampleCommand m_autonomousCommand;
 
   void ConfigureButtonBindings();
 };
